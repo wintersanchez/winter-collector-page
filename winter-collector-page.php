44c6cc0a5482ba6b158aa571231da802c854d49c
wintersanchez <winterwsanchez@gmail.com>
2021-02-09 09:28:39 +0000

    connected db to php and created basic html site with css styling to display db contents

---
+++ winter-collector-page.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once 'winter-collector-page.html';
+
+$db = new PDO ('mysql:host=db;dbname=winter-collector-page', 'root', 'password');
+
+$query = $db -> prepare("SELECT * FROM cats");
+
+$query -> execute();
+
+$allResults = $query->fetchAll(\PDO::FETCH_ASSOC);
+
+echo "<section>";
+foreach ($allResults as $row) {
+    echo "<div>";
+    echo "<ul>
+        <li><h3>Name:</h3> " . $row["name"] . "</li>" .
+        "<li><h3>Colour:</h3> " . $row["colour"] . "</li>" .
+        "<li><h3>Weight:</h3> " . $row["weight"] . "kg</li>" .
+        "<li><h3>Age:</h3> " . $row["age"] . "</li>" .
+        "<li><h3>Breed:</h3> " . $row["breed"] . "</li>
+        </ul>";
+    echo "<br>";
+    echo "<br>";
+    echo "<form method='post' action='delete'>";
+    echo "<button type='submit'>delete this cat?</button></form>";
+    echo "</div>";
+}
+echo "</section>";
+
+
+
+
+
+// TABLE??
+//echo "<table><th>Name</th><th>Colour</th><th>Weight</th><th>Age</th><th>Breed</th>";
+//
+//foreach ($allResults as $row) {
+//    echo "<tr>";
+//    echo "<th>" . $row["name"] . " | " . $row["colour"] . " | " .
+//        $row["weight"] . "kg | " . $row["age"] . " | " . $row["breed"] . "</tr>";
+//}
+//echo '</table>';
+
+// I DON'T KNOW WHAT I WAS TRYING TO DO HERE BUT OK?
+//echo "</table>";
+//
+//echo "<table>";
+//
+//// NAME
+//echo "<tr><th>Name</th>";
+//foreach ($allResults as $row) {
+//    echo "<td>" . $row["name"] . "</td>";
+//}
+//echo "</tr>";
+//
+////COLOUR
+//echo "<tr><th>Colour</th>";
+//
+//foreach ($allResults as $row) {
+//    echo "<td>" . $row["colour"] . "</td>";
+//}
+//echo "</tr>";
+//
+////BREED
+//echo "<tr><th>Breed</th>";
+//
+//foreach ($allResults as $row) {
+//    echo "<td>" . $row["breed"] . "</td>";
+//}
+//echo "</tr>";
+//
+//// AGE
+//echo "<tr><th>Age</th>";
+//
+//foreach ($allResults as $row) {
+//    echo "<td>" . $row["age"] . "years" . "</td>";
+//}
+//echo "</tr>";
+//
+////WEIGHT
+//echo "<tr><th>Weight</th>";
+//
+//foreach ($allResults as $row) {
+//    echo "<td>" . $row["weight"] . "kg" . "</td>";
+//}
+//echo "</tr>";
+//
+//echo '</table>';
